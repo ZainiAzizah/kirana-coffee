b79a40edb21968077bbee9b6c6fe42d7c25a12f3
ZainiAzizah <zainiaflahaazizah23@mhs.politala.ac.id>
2024-10-03 13:24:49 +0800

    Membuat Model Menu

---
+++ app/Models/Menu.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_menu',
+        'nama_menu',
+        'detail_menu',
+        'gambar_menu',
+        'harga',
+        'stok',
+        'id_kategori',
+    ];
+
+}
